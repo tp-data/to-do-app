47854b11b25b22ca0cede3a8ad3e434bf8fd07a9
trevor-pahigian <trevor.pahigian@deel.com>
2023-01-31 21:14:20 -0500

    add javascript functions

--- js/task_complete.js
+++ js/task_complete.js
@@ -1,76 +1,39 @@
+function myFunction() {
+  if (($('.checkbox:checked').length == $('.checkbox').length) && $('.checkbox:checked').length !== 0) {
+    const progressBar = document.getElementById('complete-container-pulse');
+    const message = document.getElementById('complete-container-message');
+    progressBar.style.visibility = `visible`;
+    message.style.visibility = `visible`;
+
+  }
+  else{
+    const progressBar = document.getElementById('complete-container-pulse');
+    const message = document.getElementById('complete-container-message');
+    progressBar.style.visibility = `hidden`;
+    message.style.visibility = `hidden`;
+
+  }
+}
+
 $(function(){
     $(document).delegate('.checkbox, .close', 'click', function() {
   
-    if (($('.checkbox:checked').length == $('.checkbox').length) && $('.checkbox:checked').length !== 0) {
-      const progressBar = document.getElementById('complete-container-pulse');
-      const message = document.getElementById('complete-container-message');
-      progressBar.style.visibility = `visible`;
-      message.style.visibility = `visible`;
-  
-    }
-    else{
-      const progressBar = document.getElementById('complete-container-pulse');
-      const message = document.getElementById('complete-container-message');
-      progressBar.style.visibility = `hidden`;
-      message.style.visibility = `hidden`;
-  
-    }
+      myFunction();
   })});
   
   $(function(){
     $('.task-form').submit(function() {
   
-    if (($('.checkbox:checked').length == $('.checkbox').length) && $('.checkbox:checked').length !== 0) {
-      const progressBar = document.getElementById('complete-container-pulse');
-      const message = document.getElementById('complete-container-message');
-      progressBar.style.visibility = `visible`;
-      message.style.visibility = `visible`;
-  
-    }
-    else{
-      const progressBar = document.getElementById('complete-container-pulse');
-      const message = document.getElementById('complete-container-message');
-      progressBar.style.visibility = `hidden`;
-      message.style.visibility = `hidden`;
-  
-    }
+      myFunction();
   })});
   
     $(function(){
       $('.checkbox, .close').on('click', function() {
-    
-      if (($('.checkbox:checked').length == $('.checkbox').length) && $('.checkbox:checked').length !== 0) {
-        const progressBar = document.getElementById('complete-container-pulse');
-        const message = document.getElementById('complete-container-message');
-        progressBar.style.visibility = `visible`;
-        message.style.visibility = `visible`;
-    
-      }
-      else{
-        const progressBar = document.getElementById('complete-container-pulse');
-        const message = document.getElementById('complete-container-message');
-        progressBar.style.visibility = `hidden`;
-        message.style.visibility = `hidden`;
-  
-      }
+        myFunction();
     })});
   
   
     $(function(){
       $( document ).ready(function() {
-    
-      if (($('.checkbox:checked').length == $('.checkbox').length) && $('.checkbox:checked').length !== 0){
-        const progressBar = document.getElementById('complete-container-pulse');
-        const message = document.getElementById('complete-container-message');
-        progressBar.style.visibility = `visible`;
-        message.style.visibility = `visible`;
-    
-      }
-      else{
-        const progressBar = document.getElementById('complete-container-pulse');
-        const message = document.getElementById('complete-container-message');
-        progressBar.style.visibility = `hidden`;
-        message.style.visibility = `hidden`;
-  
-      }
+        myFunction();
     })});    
